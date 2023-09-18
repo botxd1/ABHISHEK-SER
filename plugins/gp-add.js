8fb26e6684eb15e9f635541a971fbb6c58e54bf6
unique6412hack <98762656+unique6412hack@users.noreply.github.com>
2023-09-18 12:06:01 +0530

    Update gp-add.js

--- plugins/gp-add.js
+++ plugins/gp-add.js
@@ -50,7 +50,7 @@ let handler = async (m, { conn, text, participants }) => {
 handler.help = ['add']
 handler.tags = ['group']
 handler.command = ['add']
-handler.admin = true
+//handler.admin = true
 handler.group = true
 handler.rowner = true
 handler.botAdmin = true
