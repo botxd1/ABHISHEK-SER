a8626859ba51a30913c082874914df89e96efd1f
unique6412hack <98762656+unique6412hack@users.noreply.github.com>
2023-09-21 23:46:25 +0530

    Update main-blocklist.js

--- plugins/main-blocklist.js
+++ plugins/main-blocklist.js
@@ -17,5 +17,5 @@ let handler = async (m, { conn }) => {
 handler.help = ['blocklist']
 handler.tags = ['main']
 handler.command = ['blocklist', 'listblock'] 
-
+handler.admin = true
 export default handler
